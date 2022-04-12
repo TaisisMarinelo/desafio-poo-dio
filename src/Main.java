eca896b5097e01bb1a9140032dbde4ef70e4738c
TaisisMarinelo <taisismarinelo@gmail.com>
2022-04-11 21:18:02 -0300

    commit final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,9 +24,42 @@ public class Main {
         mentoria1.setDescricao("descrição de mentoria java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Fulano");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Fulano" + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Concluidos Fulano" + dev1.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos Fulano" + dev1.getConteudosInscritos());
+        System.out.println("XP:" + dev1.calcularTotalXp());
+
+        System.out.println("------------------------------------------------------------------------------");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Ciclano");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Ciclano" + dev2.getConteudosInscritos());
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Concluidos Ciclano" + dev2.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos Ciclano" + dev2.getConteudosInscritos());
+        System.out.println("XP:" +dev2.calcularTotalXp());
+
+
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
 
     }
 
